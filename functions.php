728ecaa7613f1c9bf5d9c5dffc08814cb52245ba
Kunal <kunaldandona009@gmail.com>
2019-12-30 01:53:31 -0800

    Styling Header

--- functions.php
+++ functions.php
@@ -7,8 +7,15 @@
 // IMPORT PARENT STYLE
 function child_theme_enqueue_styles() {
     $parent_style = 'divi-style'; // This is 'divi-style' for the Divi theme.
-    wp_enqueue_style( 'divi-override', get_stylesheet_directory_uri() . '/stylesheet/override.css' );
+
+    wp_enqueue_style( 'divi-override',
+        get_stylesheet_directory_uri() . '/stylesheet/override.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/stylesheet/style.css',
         array( $parent_style ),
