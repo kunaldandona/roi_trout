dfd882ed15b425d28ccca2bb033f6745ea33d2c1
Kunal <kunaldandona009@gmail.com>
2019-12-30 01:51:53 -0800

    Styling Header

--- functions.php
+++ functions.php
@@ -4,15 +4,6 @@
  *
  */
 
-function wpb_adding_scripts() {
-
-    wp_register_script('my_amazing_script', plugins_url('amazing_script.js', __FILE__), array('jquery'),'1.1', true);
-
-    wp_enqueue_script('my_amazing_script');
-}
-
-add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
-
 // IMPORT PARENT STYLE
 function child_theme_enqueue_styles() {
     $parent_style = 'divi-style'; // This is 'divi-style' for the Divi theme.
