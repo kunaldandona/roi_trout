ca0fd8499a5ced35a95bb84e77d039b383d022bd
Kunal <kunaldandona009@gmail.com>
2020-01-05 17:00:31 -0800

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function ($) {
     console.log('working', $('header').height());
+
+    $('.logo_container').click(function(e){
+        e.preventDefault();
+        $('#network_nav').css({ top: $('header').height() });
+    });
+
+
 });
