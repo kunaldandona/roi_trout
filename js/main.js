67ba023887458ad1db02bdc40d96a84ab1190df2
Kunal <kunaldandona009@gmail.com>
2020-01-05 18:15:32 -0800

    head

--- js/main.js
+++ js/main.js
@@ -27,8 +27,45 @@ jQuery(document).ready(function ($) {
         $.toggleArrow($.adjustHeader());
     });
 
-    $(window).scroll(function(){
-        $.adjustHeader();
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
     });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        $('#network_nav').css({ top: headerHeight });
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+
+
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
 
 });
