ac5f85e9391fec3de9c17816c514370bf5afcd48
Kunal <kunaldandona009@gmail.com>
2020-01-07 21:58:23 -0800

    footer

--- footer.php
+++ footer.php
@@ -74,7 +74,7 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
                             </div>
                             <div class="footer-col">
                                 <h2>Contact Us</h2>
-
+                                <?php gravity_form( 1, false, false, false, '', false ); ?>
                             </div>
                         </div>
 
