70f38ba11b6000ec12588f626e93e5649f46fa88
Kunal <kunaldandona009@gmail.com>
2020-01-07 18:54:59 -0800

    footer

--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@
  */
 global $troutnetwork, $current;
 
+if( function_exists('acf_add_options_page') ) {
+    acf_add_options_page();
+}
 
 
 $troutnetwork = array(
@@ -52,24 +55,4 @@ function my_theme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
 
-if( function_exists('acf_add_options_page') ) {
-    acf_add_options_page();
-}
-
-add_action('acf/init', 'my_acf_op_init');
-function my_acf_op_init() {
-
-    // Check function exists.
-    if( function_exists('acf_add_options_page') ) {
-
-        // Register options page.
-        $option_page = acf_add_options_page(array(
-            'page_title'    => __('Theme General Settings'),
-            'menu_title'    => __('Theme Settings'),
-            'menu_slug'     => 'theme-general-settings',
-            'capability'    => 'edit_posts',
-            'redirect'      => false
-        ));
-    }
-}
 ?>
