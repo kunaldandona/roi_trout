727d1db6fbe4968e1f49c32a55fa777eea4b1a64
Kunal <kunaldandona009@gmail.com>
2020-01-07 19:03:06 -0800

    footer

--- functions.php
+++ functions.php
@@ -6,7 +6,27 @@
 global $troutnetwork, $current;
 
 
+add_action('acf/init', 'my_acf_op_init');
+function my_acf_op_init() {
 
+    // Check function exists.
+    if( function_exists('acf_add_options_sub_page') ) {
+
+        // Add parent.
+        $parent = acf_add_options_page(array(
+            'page_title'  => __('Theme General Settings'),
+            'menu_title'  => __('Theme Settings'),
+            'redirect'    => false,
+        ));
+
+        // Add sub page.
+        $child = acf_add_options_sub_page(array(
+            'page_title'  => __('Social Settings'),
+            'menu_title'  => __('Social'),
+            'parent_slug' => $parent['menu_slug'],
+        ));
+    }
+}
 
 
 
