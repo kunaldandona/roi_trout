c558754b1ab18a9b715a7bbcf72a6d9a54ec3791
Kunal <kunaldandona009@gmail.com>
2020-01-19 02:00:53 -0800

    mobile responsive

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ jQuery(document).ready(function ($) {
         $('#mobile_menu .second').wrapAll('<div class="second-trout"></div>');
     }
 
+    $(function () {
+        while ($('#mobile_menu').length > 0) {
+            if ($('#mobile_menu').length > 0){
+                $('#mobile_menu .single-menu, #mobile_menu .mega-menu').wrapAll('<div class="primary-trout"></div>');
+                $('#mobile_menu .second').wrapAll('<div class="second-trout"></div>');
+            }
+            console.log('tik');
+        }
+    });
+
 
 
     var headTop = $('header').height() + $('#top-header').height();
