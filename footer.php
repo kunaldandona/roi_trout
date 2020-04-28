69f78f50d2bceb3196ea24024c67eb8746048b1d
Kunal <kunaldandona009@gmail.com>
2020-04-28 11:21:04 -0700

    footer modified

--- footer.php
+++ footer.php
@@ -84,14 +84,13 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
                                <!-- Begin Mailchimp Signup Form -->
                                 <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
                                 <style type="text/css">
-                                #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif;  width:300px;}
+                                #mc_embed_signup{background:#142d06; clear:left; font:14px Helvetica,Arial,sans-serif;  width:300px;}
                                 /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
                                 We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
                                 </style>
                                 <div id="mc_embed_signup">
                                 <form action="https://troutcreek.us19.list-manage.com/subscribe/post?u=0048b6d599888a2b50b08a112&amp;id=5d4b93aa33" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                                     <div id="mc_embed_signup_scroll">
-                                <h2>Subscribe</h2>
                                 <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
                                 <div class="mc-field-group">
                                 <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
